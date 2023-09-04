fc4e691cf85a4413ff4354d06650d696304a4e25
hansrichard2000 <hansrichard2000@gmail.com>
2023-09-04 23:29:56 +0700

    Add code for enqueue

--- app/src/main/java/com/example/waterme/data/WorkManagerWaterRepository.kt
+++ app/src/main/java/com/example/waterme/data/WorkManagerWaterRepository.kt
@@ -31,5 +31,19 @@ class WorkManagerWaterRepository(context: Context) : WaterRepository {
     override val plants: List<Plant>
         get() = DataSource.plants
 
-    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {}
+    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {
+        val data = Data.Builder()
+        data.putString(WaterReminderWorker.nameKey, plantName)
+
+        val workRequestBuilder = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(data.build())
+            .build()
+
+        workManager.enqueueUniqueWork(
+            plantName+duration,
+            ExistingWorkPolicy.REPLACE,
+            workRequestBuilder
+        )
+    }
 }
